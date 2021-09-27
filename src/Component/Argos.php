526e7fd4647fe7fba45cac38c949642b52008a5e
andyexeter <palmer.andy@gmail.com>
2021-09-27 12:57:02 +0100

    Update Argos request headers

--- src/Component/Argos.php
+++ src/Component/Argos.php
@@ -6,9 +6,18 @@ class Argos extends AbstractSite implements SiteInterface
 {
     private const HEADERS = [
         'authority' => 'www.argos.co.uk',
-        'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
+        'pragma' => 'no-cache',
+        'cache-control' => 'no-cache',
+        'sec-ch-ua' => '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+        'sec-ch-ua-mobile' => '?0',
+        'sec-ch-ua-platform' => '"Linux"',
+        'upgrade-insecure-requests' => '1',
+        'user-agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36',
         'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'referer' => 'https://www.argos.co.uk/search/ps5/?clickOrigin=searchbar:home:term:ps5',
+        'sec-fetch-site' => 'none',
+        'sec-fetch-mode' => 'navigate',
+        'sec-fetch-user' => '?1',
+        'sec-fetch-dest' => 'document',
         'accept-language' => 'en-GB,en-US;q=0.9,en;q=0.8',
     ];
 
